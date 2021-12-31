d3566c2324d71a3881d515d1b76b8ecdc5629224
Jia Jie <80145122+yjjjiajie@users.noreply.github.com>
2021-12-31 16:18:30 +0800

    Add files via upload

---
+++ revenue_and_margin.sql
@@ -0,0 +1,15 @@
+SELECT 
+	YEAR(created_at),
+    MONTH(created_at),
+    SUM(CASE WHEN product_id = 1 THEN price_usd ELSE NULL END) AS mrfuzzy_rev,
+    SUM(CASE WHEN product_id = 1 THEN price_usd-cogs_usd ELSE NULL END) AS mrfuzzy_marg,
+    SUM(CASE WHEN product_id = 2 THEN price_usd ELSE NULL END) AS lovebear_rev,
+    SUM(CASE WHEN product_id = 2 THEN price_usd-cogs_usd ELSE NULL END) AS lovebear_marg,
+    SUM(CASE WHEN product_id = 3 THEN price_usd ELSE NULL END) AS birthdaybear_rev,
+    SUM(CASE WHEN product_id = 3 THEN price_usd-cogs_usd ELSE NULL END) AS birthday_bear_marg,
+    SUM(CASE WHEN product_id = 4 THEN price_usd ELSE NULL END) AS minibear_rev,
+    SUM(CASE WHEN product_id = 4 THEN price_usd-cogs_usd ELSE NULL END) AS minibear_marg,
+    SUM(price_usd) AS rev,
+    SUM(price_usd-cogs_usd) AS marg
+FROM order_items
+GROUP BY 1,2;
